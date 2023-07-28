313c98619feacbe663d86745c1e38d413a12a5bc
Hamidreza Babaei <babaiee.hamidreza@gmail.com>
2023-07-28 15:29:13 +0330

    mergeSorting added

--- Sorting algorithms/mergeSorting.cpp
+++ Sorting algorithms/mergeSorting.cpp
@@ -1,12 +1,66 @@
 #include <iostream>
 #include <stdio.h>
+#include <vector>
 
 using namespace std;
 
 int main(){
+    int n=5 , ni = 0 , m=10 , mi = 0; 
+    cin >> n >> m;
+    int array1[n] , array2[m] ,max = 0 , min = 0;
 
-    int array1[5] = {1,4,6,7,8}, array2[10] = {1,2,3,4,5,7,9,10,11,12};
-    
+    for(int i=0 ; i<n ; i++){
+        cin >> array1[i];
+    }
+
+    for(int i=0 ; i<m ; i++){
+        cin >> array2[i];
+    }
+
+    if(n>m)min = m;
+    if(m>n)min = n;
+    int array3[m+n] = {0};
+    vector<int> array4;
+    bool finish = false;
+
+    while(finish == false){
+        
+        if(ni!=n && mi!=m && array1[ni] == array2[mi]){
+            array4.push_back(array1[ni]);
+            ni++;
+            mi++;
+            continue;
+        }
+
+        if(ni!=n && mi!=m && array1[ni] > array2[mi]){
+            array4.push_back(array2[mi]);
+            mi++;
+            continue;
+        }
+       
+        if(ni!=n && mi!=m && array1[ni] < array2[mi]){
+            array4.push_back(array1[ni]);
+            ni++;
+            continue;
+        }
+
+        if(ni==n && min != m){
+            array4.push_back(array2[mi]);
+            mi++;
+            if(mi==m)break;
+        }else if(mi == m && min != n){
+            array4.push_back(array1[ni]);
+            ni++;
+              if(ni==n)break;
+        }
+     
+    }
+
+    cout << endl << "vector : ";
+    for(const int& i : array4){
+        cout << i << " ";
+    } 
+    cout << endl ;
 
     return 0 ;
 }

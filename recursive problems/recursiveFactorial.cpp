5f3c917f303c019285d8a17e858b087b25291a9e
Hamidreza Babaei <babaiee.hamidreza@gmail.com>
2023-08-14 00:25:54 +0330

    Add recursive  functions

---
+++ recursive problems/recursiveFactorial.cpp
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <iostream>
+
+using namespace std;
+
+int factorial(int n){
+    if(n==1) return 1;
+    return factorial(n-1)*n;
+}
+
+int main(){
+    int n;
+    cin >> n;
+    cout << factorial(n) << endl;
+    return 0 ;
+}
---
+++ recursive problems/recursiveSum.cpp
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <iostream>
+
+using namespace std;
+
+int sum(int n , int arr[]){
+    if(n==-1) return 0;
+    return sum(n-1 , arr)+arr[n];
+}
+
+int main(){
+    int n;
+    cin >> n;
+    int arr[n];
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
+    }
+    
+    cout << sum(n-1,arr) << endl;
+    return 0 ;
+}

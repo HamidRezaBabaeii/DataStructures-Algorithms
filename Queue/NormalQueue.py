f6acb8e32a55cd1d540141c6f68c3fd962507529
Hamidreza Babaei <105438120+HamidRezaBabaeii@users.noreply.github.com>
2023-09-15 09:27:22 +0330

    Update NormalQueue.py

--- Queue/NormalQueue.py
+++ Queue/NormalQueue.py
@@ -8,6 +8,7 @@ class Queue:
 
     def enqueue(self , value):
         self._pointerFront += 1
+
         if(self._pointerFront!=self._size):
             self.array[self._pointerFront] = value
         else:
@@ -15,18 +16,45 @@ class Queue:
         
 
     def dequeue(self):
+        if(self.array[self._pointerRear]== None):
+            print("\tThe Queue Is Empty.")
+
         if(self._pointerRear>=0 and self._pointerRear<self._size and self._pointerFront!=-1):
             self._pointerRear += 1
 
 
     def showFirstElemnt(self):
-        print(self.array[self._pointerRear])
+        print("\t",self.array[self._pointerRear])
+
+
+    def showLatestElement(self):
+        print("\t",self.array[self._pointerFront])
 
 
 queue = Queue(10)
+while(True):
+    todos = ("""
+        1 - Enqueue 
+        2 - Dequeue
+        3 - First Element
+        4 - Latest Element
+        5 - Exit
+    """)
+    command = int(input(todos+"\n\t"))
 
-queue.enqueue(5)
-queue.showFirstElemnt()
-queue.dequeue()
-queue.showFirstElemnt()
+    if(command == 1):
+        value = int(input("\tEnter value\n\t"))
+        queue.enqueue(value)
+    elif(command == 2):
+        queue.dequeue()
+    elif(command == 3):
+        queue.showFirstElemnt()
+    elif(command == 4):
+        queue.showLatestElement()
+    elif(command == 5):
+        print("\tGoodBye Baby! :)")
+        break
+    else :
+        print("Please Enter Number From Numbers Above.")
+
 
